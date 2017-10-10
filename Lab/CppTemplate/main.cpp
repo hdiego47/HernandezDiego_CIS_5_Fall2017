1f6bd27cff8285fb5c7e870091b1855c5f0f3ac4
hdiego47 <dhernandez206@student.rccd.edu>
2017-10-10 12:31:45 -0700

    Signed-off-by: hdiego47 <dhernandez206@student.rccd.edu>

---
+++ Lab/CppTemplate/main.cpp
@@ -0,0 +1,32 @@
+/* 
+ * File:   main.cpp
+ * Author: Diego Hernandez
+ * Created on August 29, 2017, 12:01 PM
+ * Purpose: Creating our class template
+ */
+
+//System Libraries
+#include <iostream>  //Input/output Stream Library
+using namespace std; //Standard Name-space under which system Libraries Reside
+
+//User Libraries
+
+//Global Constants - No variables only Math/Science/Conversion constants
+
+//Function Prototypes
+
+//Execution Begins Here
+int main(int argc, char** argv) {
+    //Declare Variables
+    
+    //Initialize Variables
+    
+    //Input Data/Variables
+    
+    //Process or map the inputs to the outputs
+    
+    //Display/Output all pertinent variables
+    
+    //Exit the program
+    return 0;
+}
